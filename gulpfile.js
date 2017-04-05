34927978ff84cb45ee250e8a97388ff045d18b35
Johann Montet <someemail@example.com>
2017-04-06 07:10:15 +1000

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function(){ //log could any name we want!
-  gutil.log('workflows are awesome');
+var coffeeSources = ['components/coffee/tagline.coffee'];
 
 
+gulp.task('coffee', function(){
+  // specifiy which file to process
+  gulp.src(coffeeSources)
+    .pipe(coffee({bare: true}) //bare : true is a coffee-script language stuff
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts')) //defines where to put file when done
 });
-
